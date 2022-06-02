8f26593fe8efb5b48fdcd3aadd4e35f8530c0d21
damiano.barbati <damiano.barbati@gmail.com>
2022-06-02 20:10:24 +0200

    var expand support (sh style), mocha spec

---
+++ src/expand.js
@@ -0,0 +1,23 @@
+/*
+test set on regex101:
+$HELLO${HOLA}${SALUT:-salut}
+${SALUT:-salut}$HELLO$HOLA
+${SALUT:-salut}\$HELLO$HOLA\$done
+*/
+const expand = (key, value, envs) => {
+  const expanded_value = value.replaceAll(/(\\?\${?[A-Z0-9_]+(:-([^}]+))?}?)/gi, (match) => {
+    // if \$ then do not expand
+    if (match.startsWith('\\')) {
+      return match.slice(1);
+    }
+
+    const { key, default_value } = /\${?(?<key>[A-Z0-9_]+)(:-(?<default_value>[^}]+))?}?/gi.exec(match).groups;
+
+    const replacement = envs[key] ?? default_value;
+    return replacement;
+  });
+
+  return expanded_value;
+};
+
+module.exports = expand;
---
+++ src/index.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const expand = require('./expand.js');
+
+const envk = () => {
+  const { NODE_ENV, ENVK, ENVK_DEBUG } = process.env;
+
+  let env_file;
+
+  if (ENVK) {
+    env_file = ENVK;
+  } else if (fs.existsSync(`.env.${NODE_ENV}`)) {
+    env_file = `.env.${NODE_ENV}`;
+  } else if (fs.existsSync(`.env`)) {
+    env_file = `.env`;
+  } else {
+    throw new Error(`[envk] Neither .env.${NODE_ENV} or .env file was found.`);
+  }
+
+  const lines = fs
+    .readFileSync(env_file, 'utf8') // read env file
+    .split('\n') // split file in lines
+    .filter(Boolean) // ignore empty lines
+    .filter((line) => !line.startsWith('#')); // ignore comments
+
+  const envs = {};
+
+  for (const index in lines) {
+    const line = lines[index];
+
+    try {
+      // keys with empty values (eg: HELLO= ) are assigned empty string
+      let [, key, value = ''] = line.match(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/);
+
+      const is_single_quoted = value.startsWith("'") && value.endsWith("'");
+      const is_double_quoted = value.startsWith('"') && value.endsWith('"');
+
+      // remove quoting
+      if (is_single_quoted || is_double_quoted) value = value.slice(1, -1);
+
+      // if double quoted then expand newlines
+      if (is_double_quoted) value = value.replace(/\\n/g, '\n');
+
+      // remove spaces
+      value = value.trim();
+
+      envs[key] = value;
+    } catch (error) {
+      console.log(error);
+      throw new Error(`ENVK line malformed in ${env_file}:${line} for key ${index}`);
+    }
+  }
+
+  // expand variables
+  for (const [key, value] of Object.entries(envs)) {
+    if (value.includes('$')) {
+      envs[key] = expand(key, value, envs);
+    }
+  }
+
+  if (ENVK_DEBUG) {
+    console.log(`[envk] reading envs from ${env_file}`);
+    for (const [key, value] of Object.entries(envs)) {
+      console.log(`[envk] ${key}=${value}`);
+    }
+  }
+
+  Object.assign(process.env, envs);
+
+  return envs;
+};
+
+module.exports = envk;
+
+// if script is invoked via "-r envk" then auto execute envk()
+if (require.main === module) envk();
