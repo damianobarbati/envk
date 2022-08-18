50521a64d1f42db9480f8924d34bdc6101787c27
damiano.barbati <damiano.barbati@gmail.com>
2022-08-18 23:05:10 +0200

    deps bump, better actions

--- src/envk.spec.js
+++ src/envk.spec.js
@@ -1,27 +1,13 @@
-const fs = require('fs');
+process.env.ENVK = 'examples/.env';
+
 const { strict: assert } = require('assert');
 const envk = require('./envk.js');
 
-const env = `
-HELLO=world
-HOLA='mundo'
-CIAO="mondo 😀"
-# rere
-EXPANDED=\${HELLO}-\${HOLA}imfinehere\${SALUT:-salut}\\$done
-
-KEY=abc1
-KEY=abc2
-`;
-
 it('works', () => {
-  fs.writeFileSync('.env', env);
-
   const result = envk();
 
   assert.equal(result.HELLO, 'world');
   assert.equal(result.HOLA, 'mundo');
   assert.equal(result.CIAO, 'mondo 😀');
   assert.equal(result.EXPANDED, 'world-mundoimfineheresalut$done');
-
-  fs.unlinkSync('.env');
 });
